4319f3262101d89cefcfca85627c60192d58628e
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2018-08-07 17:00:03 +0200

    trying to handle events

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -2,23 +2,20 @@ AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         // Set up the tick throttling. Will check if marker is active every 500ms
+        this.addedListener = false;
         this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
     },
 
     tick: function(t, dt) {
         const animatedMarker = document.querySelector("#animated-marker");
-        if (animatedMarker.object3D.visible == true) {
-
-            const handler = function(){
-                onClickCheck(() => alert('CLICK!'));
-            };
-
-            if (!this.handler) {
-                animatedMarker.addEventListener('click', );
-                this.handler = handler;
+        if (animatedMarker.object3D.visible == true && !this.addedListener) {
+                animatedMarker.addEventListener('click', () => {
+                    alert('CLICK!');
+                });
+                this.addedListener = true;
             }
         }
-}});
+});
 
 
 
