de8f7b236f6d778669583c72580d424ccf096fc3
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2018-08-07 12:21:41 +0200

    trying to handle events

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -2,10 +2,12 @@ AFRAME.registerComponent('markerhandler', {
 
   init: function() {
     // Set up the tick throttling. Will check if marker is active every 500ms
+    console.log('intanto entro qua', AFRAME, AFRAME.utils)
     this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
   },
 
   tick: function(t, dt) {
+      console.log('enntroa qua??', t,dt)
     if (document.querySelector("#animated-marker").object3D.visible == true) {
         // MARKER IS PRESENT
         //alert("MARKER IS PRESENT")
