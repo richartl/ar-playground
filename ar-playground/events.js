da78b09a3bb1fcd49e780797fb380bea789d61f9
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2018-08-07 14:10:19 +0200

    trying to handle events

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -9,10 +9,8 @@ AFRAME.registerComponent('markerhandler', {
     const animatedMarker = document.querySelector("#animated-marker");
     var animatedObject = document.querySelector('#animated-model');
     if (animatedMarker.object3D.visible == true || animatedObject.object3D.visible == true) {
-        // MARKER IS PRESENT
         console.log('s', 'AAA', t, dt,);
         // var animatedObject = document.querySelector('#animated-model');
-        console.log('AAA', t, dt, animatedObject);
         animatedObject.addEventListener('click', function() {
             alert("clicconeee")
         });
