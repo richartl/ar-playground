d7a75b7cfdebc24ad0b8a24a7f81f35f733b089a
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2018-08-08 09:59:47 +0200

    trying to handle events

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -5,9 +5,8 @@ AFRAME.registerComponent('markerhandler', {
 
         // every click, we make our model grow in size
         animatedMarker.addEventListener('click', function(ev, target){
-            if (animatedMarker.object3D.visible == true) {
+            if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
                 const entity = document.querySelector('#animated-model');
-                console.log(ev.detail, entity && entity.object3D)
                 const scale = entity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
                 entity.setAttribute('scale', scale);
