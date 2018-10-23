09c183b5acadefab1773795e09e25d6b9d7080d0
Nicolò <>
2018-10-23 19:42:29 +0200

    feat: added fuse false

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -5,7 +5,9 @@ AFRAME.registerComponent('markerhandler', {
 
         // every click, we make our model grow in size
         animatedMarker.addEventListener('click', function(ev, target){
+            console.log('outside', ev)
             if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
+                console.log('dentrone', ev)
                 const entity = document.querySelector('#animated-model');
                 const scale = entity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
